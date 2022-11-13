ac86ea692e9d18ee4d38ddd83034ae7e192c8f70
ketrab <bartekkrawisz@wp.pl>
2022-11-13 14:18:02 +0100

    fix install bug

--- install.sh
+++ install.sh
@@ -3,9 +3,10 @@
 # This script should be run via curl:
 # sh -c "$(curl -fsSL https://raw.githubusercontent.com/ketrab2003/light-linux/master/install.sh)"
 
+# download only neccesary files
 curl -fsSL https://raw.githubusercontent.com/ketrab2003/light-linux/master/setup_all.sh > setup_all.sh
 curl -fsSL https://raw.githubusercontent.com/ketrab2003/light-linux/master/setup_system.sh > setup_system.sh
-chmod +x setup_all.sh
-chmod +x setup_system.sh
+curl -fsSL https://raw.githubusercontent.com/ketrab2003/light-linux/master/packages.txt > packages.txt
+chmod +x setup_all.sh setup_system.sh
 
 ./setup_all.sh
