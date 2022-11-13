ed7de7e4387937bf85ca794b0ddacdc0ac6d47ed
ketrab <bartekkrawisz@wp.pl>
2022-11-13 14:35:47 +0100

    run zsh install as user

--- setup_zsh.sh
+++ setup_zsh.sh
@@ -2,16 +2,16 @@
 # This script requires env variables set in setup_all.sh
 
 # install oh-my-zsh (from https://ohmyz.sh/)
-RUNZSH=no CHSH=no sh -c "$(curl -fsSL https://raw.github.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
+sudo -u "${SETUP_USERNAME}" RUNZSH=no CHSH=no sh -c "$(curl -fsSL https://raw.github.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
 
 # install zsh-autosuggestions (from https://github.com/zsh-users/zsh-autosuggestions/blob/master/INSTALL.md)
-git clone https://github.com/zsh-users/zsh-autosuggestions ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-autosuggestions
+sudo -u "${SETUP_USERNAME}" git clone https://github.com/zsh-users/zsh-autosuggestions ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-autosuggestions
 
 # create link to .zshrc
 rm -rf /home/${SETUP_USERNAME}/.zshrc
 ln -s "$(pwd)/dotfiles/.zshrc" "/home/${SETUP_USERNAME}/"
 
 # change default shell
-chsh -s "/usr/bin/zsh" "$SETUP_USERNAME"
+chsh -s "/usr/bin/zsh" "${SETUP_USERNAME}"
 
 echo "Done setting up zsh!"
