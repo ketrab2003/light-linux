495f52347e2e800025aa13d78d7f92bd5bcbc7f4
ketrab <bartekkrawisz@wp.pl>
2022-11-13 12:49:57 +0100

    Export setup variables

--- setup_all.sh
+++ setup_all.sh
@@ -1,9 +1,9 @@
 #!/bin/sh
 
-SETUP_DRIVE=/dev/sda
-SETUP_USERNAME=ketrab
-SETUP_PASSWORD=1
-SETUP_LOCATION=/home/${SETUP_USERNAME}/system_config  # absolute path, in new filesystem
+export SETUP_DRIVE=/dev/sda
+export SETUP_USERNAME=ketrab
+export SETUP_PASSWORD=1
+export SETUP_LOCATION=/home/${SETUP_USERNAME}/system_config  # absolute path, in new filesystem
 
 exec ./setup_system.sh
 
@@ -12,8 +12,8 @@ mkdir -p /mnt${SETUP_LOCATION}
 cp -r * /mnt/${SETUP_LOCATION}/
 
 arch-chroot /mnt /bin/bash <<EOF
-  SETUP_USERNAME=${SETUP_USERNAME}
-  SETUP_PASSWORD=${SETUP_PASSWORD}
+  export SETUP_USERNAME=${SETUP_USERNAME}
+  export SETUP_PASSWORD=${SETUP_PASSWORD}
 
   cd ${SETUP_LOCATION}
 
